9f1861e335699817ddd909a97616b8c763bb261f
Ernest Fernandez <ernestgilfernandez@gmail.com>
2024-11-21 12:29:03 +0800

    Initialized stores views.py file and template directory

--- inventorysystem/stores/views.py
+++ inventorysystem/stores/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.views import View
+from django.views.generic.edit import FormView
+
+from .forms import ProductForm
+
 # Create your views here.
+class ProductFormView(FormView):
+    pass
