070cc000908cba83627da9fa05fcdb1441fb5ff7
Ernest Fernandez <ernestgilfernandez@gmail.com>
2024-11-24 18:46:00 +0800

    Added Store link to Navbar

--- inventorysystem/stores/urls.py
+++ inventorysystem/stores/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("catalog/", views.ProductListView.as_view(), name="product_list"),
+    path("", views.ProductListView.as_view(), name="product_list"),
 ]
